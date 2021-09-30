0800175288d36fab7b8fa57e4b6aeda7624cb334
Henrique <henrique.faria@baculasystems.com>
2021-09-30 11:22:57 -0300

    initial android studio project commit

---
+++ app/src/main/java/com/hfaria/portfolio/codewars/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hfaria.portfolio.codewars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hfaria.portfolio.codewars.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
