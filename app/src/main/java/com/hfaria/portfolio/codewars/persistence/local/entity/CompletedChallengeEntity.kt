805e96f4e5652074483259bc8636e23d4a3c84a4
Henrique <henrique.faria@baculasystems.com>
2021-10-06 18:33:47 -0300

    Persistence: - Added onDelete CASCADE police to user's completed challenges

--- app/src/main/java/com/hfaria/portfolio/codewars/persistence/local/entity/CompletedChallengeEntity.kt
+++ app/src/main/java/com/hfaria/portfolio/codewars/persistence/local/entity/CompletedChallengeEntity.kt
@@ -5,13 +5,12 @@ import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.hfaria.portfolio.codewars.persistence.remote.api.UserEntity
 
-//@Entity(tableName = "completed_challenge", foreignKeys = arrayOf(
-//    ForeignKey(entity = UserEntity::class,
-//        parentColumns = arrayOf("username"),
-//        childColumns = arrayOf("author"),
-//        onDelete = ForeignKey.CASCADE)
-//))
-@Entity(tableName = "completed_challenge")
+@Entity(tableName = "completed_challenge", foreignKeys = arrayOf(
+    ForeignKey(entity = UserEntity::class,
+        parentColumns = arrayOf("username"),
+        childColumns = arrayOf("author"),
+        onDelete = ForeignKey.CASCADE)
+))
 data class CompletedChallengeEntity(
     @PrimaryKey
     var id: String,
