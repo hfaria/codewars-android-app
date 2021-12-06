68b26960ca9665b43ea29c5273d02d49975d54cb
Henrique Faria <hfaria020@gmail.com>
2021-12-06 20:03:21 -0300

    ATDD / Testing Strategy: created BaseAcceptanceTest class

--- app/src/test/java/com/hfaria/portfolio/codewars/acceptance/search_user/SearchUserAcceptanceTest.kt
+++ app/src/test/java/com/hfaria/portfolio/codewars/acceptance/search_user/SearchUserAcceptanceTest.kt
@@ -1,41 +1,14 @@
 package com.hfaria.portfolio.codewars.acceptance.search_user
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.hfaria.portfolio.codewars.test_setup.*
 import com.hfaria.portfolio.codewars.ui.search_user.NewSearchUserViewModel
 import junit.framework.Assert.assertEquals
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import javax.inject.Inject
 
-@ExperimentalCoroutinesApi
-class SearchUserAcceptanceTest {
-
-    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-
-    @ExperimentalCoroutinesApi
-    @Before
-    fun setupDispatcher() {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @ExperimentalCoroutinesApi
-    @After
-    fun tearDownDispatcher() {
-        Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
-    }
-
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+class SearchUserAcceptanceTest : BaseAcceptanceTest() {
 
     @Before
     fun setup() {
@@ -54,7 +27,7 @@ class SearchUserAcceptanceTest {
     lateinit var stubApi: StubCodeWarsApi
 
     @Test
-    fun test_search_user_should_route_to_user_profile_screen() = runBlockingTest {
+    fun test_search_user_should_route_to_user_profile_screen() = runBlocking {
         val username = "g964"
         viewModel.username.postValue(username)
         viewModel.handleUserSearch()
---
+++ app/src/test/java/com/hfaria/portfolio/codewars/test_setup/BaseAcceptanceTest.kt
@@ -0,0 +1,35 @@
+package com.hfaria.portfolio.codewars.test_setup
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+
+open class BaseAcceptanceTest {
+
+    // LiveData adapter code
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Coroutines adapter code
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setupDispatcher() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @ExperimentalCoroutinesApi
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+}
