fd3eff1db9ca27d5c24aa6141ab97da38f12d1e2
Henrique Faria <hfaria020@gmail.com>
2021-12-07 00:43:08 -0300

    ATDD / Testing Strategy: cleaned up unused code in ATDD presentation

--- app/src/test/java/com/hfaria/portfolio/codewars/test_setup/StubCodeWarsApi.kt
+++ app/src/test/java/com/hfaria/portfolio/codewars/test_setup/StubCodeWarsApi.kt
@@ -1,8 +1,6 @@
 package com.hfaria.portfolio.codewars.test_setup
 
-import com.hfaria.portfolio.codewars.domain.ChallengeProfile
 import com.hfaria.portfolio.codewars.persistence.DataWrapper
-import com.hfaria.portfolio.codewars.persistence.remote.api.AuthoredChallenges
 import com.hfaria.portfolio.codewars.persistence.remote.api.CodeWarsApi
 import com.hfaria.portfolio.codewars.domain.User
 
